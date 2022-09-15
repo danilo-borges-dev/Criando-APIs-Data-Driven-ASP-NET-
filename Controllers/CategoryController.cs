5d52d008454e98d5d6843f2c54aa2d74a18b9cb9
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-15 13:14:23 -0300

    Praticando a criação de Models

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -3,13 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 // Endpoint == URL
 // http://localhost/5000
 // https://localhost/5001 - Produção usar sempre o https
-// https://meuapp.azurewebsites.net/ (Quando for para o meu Servidor terei o meu domínio como base meuapp.nomedodominio.com)
+// https://meuapp.azurewebsites.net/ 
+// (Quando for para o meu Servidor terei o meu domínio como base meuapp.nomedodominio.com)
 
-// https://localhost/5001/banana/madura
-[Route("banana")]
+// https://localhost/5001/categories/
+[Route("categories")]
 public class CategoryController : ControllerBase 
 {
-    [Route("madura")]
+    [Route("")]
     public string MeuMetodo()
     {
         return "Olá Mundo!";
