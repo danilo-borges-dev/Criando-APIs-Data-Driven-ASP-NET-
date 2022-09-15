001be30d774209405a1dc8a08fe03ce174bccb30
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-15 11:25:09 -0300

    Praticando a criação de Models

--- Models/Products.cs
+++ Models/Products.cs
@@ -16,7 +16,7 @@ namespace Shop.Models
         public string Description { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
-        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+        [Range(0.1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
         public decimal Price { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
