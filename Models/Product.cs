2300bfe50fed2d966fc787d7b9fbe4b9488c2892
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-19 13:06:49 -0300

    Inciado o Banco de Dados em Memoria

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Shop.Models
 {
-    public class Products 
+    public class Product
     {
         [Key]
         public int Id { get; set; }
