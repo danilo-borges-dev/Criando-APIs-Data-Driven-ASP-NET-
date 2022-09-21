9f5cf0accd8395558575f728bc8a1f68cc31c269
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-21 09:03:19 -0300

    Conexao com o Banco InMemory

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+
+#region Conexão Conexão com o Banco de Dados
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseInMemoryDatabase("Database");    
 });
-builder.Services.AddScoped<DataContext, DataContext>();
+builder.Services.AddScoped<DataContext, DataContext>(); // <- Fecha a Conexão com o Banco de Dados
+// Acima - O bloco de Conexão com o Banco de Dados
+#endregion
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
