489c5bce0bd56e94149a9d1f51cabb063bb49278
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-27 11:18:55 -0300

    Classe Produtos

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Shop.Data;
@@ -17,7 +14,35 @@ namespace Shop.Controller
             [FromServices] DataContext context
         )
         {
-            var products = await context.Produtcs.Include(x => x.Category).AsNoTracking().ToListAsync();
+            var products = await context
+                .Produtcs.Include(x => x.Category)
+                .AsNoTracking()
+                .ToListAsync();
+            return products;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetId([FromServices] DataContext context, int id)
+        {
+            var products = await context
+                .Produtcs
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return products;
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id)
+        {
+            var products = await context
+                .Produtcs
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .Where(x => x.CategoryId ==id)
+                .ToListAsync();
             return products;
         }
     }
